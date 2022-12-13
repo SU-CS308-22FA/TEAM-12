4f6d61bb84d6f747c3026fe274a3d3b20b29489d
Ege Durum <durumege@sabanciuniv.edu>
2022-12-13 20:18:50 +0300

    fixing2

--- server/index.js
+++ server/index.js
@@ -5,25 +5,18 @@ import userRouter from "./Routers/userRouter.js";
 import matchRouter from "./Routers/matchRouter.js";
 import refereeRouter from "./Routers/refereeRouter.js";
 import cors from "cors";
-import path from "path";
 
 dotenv.config();
 
 const app = express();
 
-const __dirname = path.resolve();
 app.use(cors());
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/matches", matchRouter);
 app.use("/referees", refereeRouter);
-app.use(express.static(path.join(__dirname + "/public")));
 
-app.use((req, res, next) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
-
-app.listen(process.env.PORT || 5000, () => {
+app.listen(5000, () => {
   // connect to database
   mongoose
     .connect(process.env.DB_CONNECTION_STRING)
