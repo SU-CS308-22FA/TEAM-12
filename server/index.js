2cdb3ca0a1e60acd06d17ca3700b80620f116430
Ege Durum <durumege@sabanciuniv.edu>
2022-12-14 22:46:49 +0300

    denemeler4

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express  from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import userRouter from "./Routers/userRouter.js";
+//import userRouter from "./Routers/userRouter.js";
 import matchRouter from "./Routers/matchRouter.js";
 import refereeRouter from "./Routers/refereeRouter.js";
 import cors from "cors";
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.static(path.join(__dirname + "/public")));
 app.use(cors());
 app.use(express.json());
-//app.use("/users", userRouter);
+app.use("/users", userRouter);
 app.use("/matches", matchRouter);
 app.use("/referees", refereeRouter);
 app.use((req, res, next) => {
